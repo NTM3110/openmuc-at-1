4b0c150359cf823ed6e958aa6978578b8e7b1472
shou <mdmt3110@gmail.com>
2025-11-14 09:51:23 +0700

    update string

--- projects/app/simpledemo/src/main/java/org/openmuc/framework/app/simpledemo/SimpleDemoApp.java
+++ projects/app/simpledemo/src/main/java/org/openmuc/framework/app/simpledemo/SimpleDemoApp.java
@@ -79,20 +79,22 @@ public final class SimpleDemoApp {
     private void getChannel(){
         // logger.info("Getting value from channel {}", CHANNEL);
         // try {
-            for(int i = 0; i < 100; i++){
 
-                Value voltage = dataAccessService.getChannel("str1_cell"+ (i+1)+"_V").getLatestRecord().getValue();
-                Value temperature = dataAccessService.getChannel("str1_cell"+ (i+1)+"_T").getLatestRecord().getValue();
-                Value resistance = dataAccessService.getChannel("str1_cell"+ (i+1)+"_R").getLatestRecord().getValue();
-                if (voltage == null || temperature == null || resistance == null) {
-                    logger.warn("Channel str1_cell{}_V or str1_cell{}_T or str1_cell{}_R not found!", i+1, i+1, i+1);
-                    continue;
+            for(int i = 0; i < 2; i++){
+                for(int j = 0; j < 100; j++){
+                    Value voltage = dataAccessService.getChannel("str" +(i+1)+ "_cell"+ (j+1)+"_V").getLatestRecord().getValue();
+                    Value temperature = dataAccessService.getChannel("str" +(i+1)+ "_cell"+ (j+1)+"_V").getLatestRecord().getValue();
+                    Value resistance = dataAccessService.getChannel("str" +(i+1)+ "_cell"+ (j+1)+"_V").getLatestRecord().getValue();
+                    if (voltage == null || temperature == null || resistance == null) {
+                        logger.warn("Channel str{}_cell{}_V or str{}_cell{}_T or str{}_cell{}_R not found!", i+1, j+1, i+1, j+1, i+1, j+1);
+                        continue;
+                    }
+                    logger.info("str{}_cell_{}, Value: --------> V: {},  T: {},   R: {}", i+1, j+1, 
+                        voltage.asDouble(),
+                        temperature.asDouble(),
+                        resistance.asDouble()
+                    );
                 }
-                logger.info("str1_cell_{}, Value: --------> V: {},  T: {},   R: {}", i+1, 
-                    voltage.asDouble(),
-                    temperature.asDouble(),
-                    resistance.asDouble()
-                );
             }
         // } catch (Exception e) {
         //     logger.warn("Error updating channel {}", e.getMessage());
